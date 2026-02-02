85652e51369ef9463086956d0d978fad310d5d40
Eric Garcia <egarcia@parallelworks.com>
2026-02-02 15:39:15 -0600

    debug log

--- internal/tracker/tracker.go
+++ internal/tracker/tracker.go
@@ -209,6 +209,13 @@ func ProcessJob(cfg *config.Config, job *slurm.Job, stateDriver *state.Driver, p
 		return nil
 	}
 
+	// print info
+	log.Info().
+		Any("usage_event", usageEvent).
+		Str("sku", skuCode).
+		Str("allocation", allocationName).
+		Str("organization", cfg.OrganizationName).
+		Msg("Creating usage event")
 	// Post the usage event using the allocation from config mapping
 	resp, err := pwClient.CreateUsageEventWithResponse(context.Background(), cfg.OrganizationName, allocationName, usageEvent)
 	if err != nil {
